97cf81f623471da71b192bdf3a1f0c18b8fdc285
mizanff <mrmizanurrahman626@gmail.com>
2025-03-13 03:17:39 -0500

    all project finished

--- script/index.js
+++ script/index.js
@@ -1,3 +1,20 @@
+const showLoader = () => {
+  document.getElementById("loader").classList.remove("hidden");
+  document.getElementById("video-container").classList.add("hidden");
+};
+
+const hideLoader = () => {
+  document.getElementById("loader").classList.add("hidden");
+  document.getElementById("video-container").classList.remove("hidden");
+};
+
+function removeActiveClass() {
+  const activeButtons = document.getElementsByClassName("active");
+  for (let btn of activeButtons) {
+    btn.classList.remove("active");
+  }
+}
+
 function loadCategories() {
   // 1-fetch then data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -7,19 +24,53 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
-function loadVideos() {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos(searchText = "") {
+  showLoader();
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
+  )
     .then((response) => response.json())
     .then((data) => displayVideos(data.videos));
 }
 
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/aaac`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+//------------------------------------------
+const displayVideoDetails = (video) => {
+  console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details_container");
+  detailsContainer.innerHTML = `
+ <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      
+    </div>
+  </div>
+</div>
+  `;
+};
+
 const loadCategoryVideos = (id) => {
+  showLoader();
   const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
   console.log(url);
 
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      removeActiveClass();
       const clickedButton = document.getElementById(`btn-${id}`);
       clickedButton.classList.add("active");
       console.log(clickedButton);
@@ -77,6 +128,7 @@ const displayVideos = (videos) => {
         </h2>
       </div>
     `;
+    hideLoader();
     return;
   }
   videos.forEach((video) => {
@@ -85,7 +137,9 @@ const displayVideos = (videos) => {
     videoCard.innerHTML = `
       <div class="card bg-base-100">
         <figure class="relative">
-          <img class ="w-full h-[150px] object-cover" src="${video.thumbnail}" />
+          <img class ="w-full h-[150px] object-cover" src="${
+            video.thumbnail
+          }" />
           <span
             class="absolute bottom-2 right-2 text-white bg-black px-2 text-sm rounded"
             >3hrs 56 min ago</span
@@ -107,19 +161,30 @@ const displayVideos = (videos) => {
             <h2 class="text-sm font-semibold">${video.title}</h2>
             <p class="text-sm text-gray-400 flex gap-1">
               ${video.authors[0].profile_name}
-              <img
+              ${
+                video.authors[0].verified == true
+                  ? `<img
                 class="w-5 h-5"
                 src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
-                alt=""
-              />
+                alt=""/>`
+                  : ` `
+              }
             </p>
             <p class="text-sm text-gray-400">${video.others.views}</p>
           </div>
         </div>
+        <button onclick=loadVideoDetails("${
+          video.video_id
+        }") class="btn btn-block">Show Details</button>
       </div>
     `;
     videoContainer.append(videoCard);
   });
+  hideLoader();
 };
 
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  loadVideos(input);
+});
 loadCategories();
